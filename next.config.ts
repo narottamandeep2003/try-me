d24c03eda17f014696cfc3794a808f106fefba34
mandeep narotta <narotta2003@gmail.com>
2025-04-22 11:30:09 +0530

    Fixed Build issue

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"], // Add Cloudinary to the allowed domains
   },
-  api: {
-    bodyParser: false, // Explicitly disable the default body parser
+  reactStrictMode: true,
+  env: {
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
   },
 };
 
